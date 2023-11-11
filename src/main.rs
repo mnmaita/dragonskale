480fb664d3c867f01ceaf0c26e7f1d12197cb132
mnmaita <47983254+mnmaita@users.noreply.github.com>
2023-11-11 22:36:59 +0100

    Adds audio module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use audio::AudioPlugin;
 use bevy::prelude::*;
+
+mod audio;
 
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins((DefaultPlugins, AudioPlugin));
 
     app.run();
 }
