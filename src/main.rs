bac57813540d6e82bb55e1fbacd52ebe7a6a4f09
mnmaita <47983254+mnmaita@users.noreply.github.com>
2023-11-12 00:36:47 +0100

    Fixes noisy WGPU warnings

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,19 @@ mod textures;
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins((DefaultPlugins, AudioPlugin, TexturesPlugin));
+    app.add_plugins((
+        // FIXME: Remove setting the backend explicitly to avoid noisy warnings
+        // when https://github.com/gfx-rs/wgpu/issues/3959 gets fixed.
+        DefaultPlugins.set(RenderPlugin {
+            render_creation: RenderCreation::Automatic(WgpuSettings {
+                backends: Some(Backends::DX12),
+                ..default()
+            }),
+        }),
+        AudioPlugin,
+        TexturesPlugin,
+    ));
+
 
     app.run();
 }
