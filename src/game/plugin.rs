514954e258c0c55cc0c53ca5a4e317ea40de2dda
mnmaita <47983254+mnmaita@users.noreply.github.com>
2023-11-12 03:30:20 +0100

    Adds a placeholder background image

--- src/game/plugin.rs
+++ src/game/plugin.rs
@@ -8,6 +8,16 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::InGame), spawn_player);
+        app.add_systems(OnEnter(AppState::InGame), (draw_background, spawn_player));
     }
+}
+
+fn draw_background(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let texture = asset_server
+        .get_handle("textures/background.png")
+        .unwrap_or_default();
+    commands.spawn(SpriteBundle {
+        texture,
+        ..default()
+    });
 }
