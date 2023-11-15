b25bc5f5194172a9e5743704f353df6a56fc88df
Martín Maita <47983254+mnmaita@users.noreply.github.com>
2023-11-14 22:00:25 -0300

    Random Level Generation (#63)
    Issue:
    ==============
    Closes #54
    Closes #9
    
    What was done:
    ==============
    * Added a random level generator that uses Perlin noise.
    * Constrains the camera within the level boundaries.
    * Added Tile and BorderTile components.

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::game::Player;
+use crate::{
+    game::Player,
+    level::{GRID_SIZE, HALF_TILE_SIZE, TILE_SIZE},
+};
 
 pub struct CameraPlugin;
 
@@ -8,7 +11,14 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_camera);
 
-        app.add_systems(Update, update_camera.run_if(any_with_component::<Player>()));
+        app.add_systems(
+            Update,
+            (
+                update_camera.run_if(any_with_component::<Player>()),
+                constrain_camera_position_to_level,
+            )
+                .chain(),
+        );
     }
 }
 
@@ -25,4 +35,50 @@ fn update_camera(
 
     camera_transform.translation.x = player_transform.translation.x;
     camera_transform.translation.y = player_transform.translation.y;
+}
+
+fn constrain_camera_position_to_level(
+    mut camera_query: Query<(&Camera, &mut Transform), With<Camera2d>>,
+    #[cfg(debug_assertions)] mut gizmos: Gizmos,
+) {
+    let (camera, mut camera_transform) = camera_query.single_mut();
+
+    if let Some(viewport_size) = camera.logical_viewport_size() {
+        let level_dimensions = GRID_SIZE * TILE_SIZE;
+        let viewport_size_remainder = viewport_size % TILE_SIZE;
+        let camera_boundary_size = (level_dimensions
+            - (viewport_size - viewport_size_remainder)
+            - viewport_size_remainder)
+            .clamp(Vec2::ZERO, Vec2::splat(f32::MAX));
+        let camera_boundary = Rect::from_center_size(-HALF_TILE_SIZE, camera_boundary_size);
+
+        if camera_boundary.is_empty() {
+            if viewport_size.x > level_dimensions.x {
+                camera_transform.translation.x = 0.0;
+            }
+            if viewport_size.y > level_dimensions.y {
+                camera_transform.translation.y = 0.0;
+            }
+        }
+
+        if camera_boundary.size() != Vec2::ZERO
+            && !camera_boundary.contains(camera_transform.translation.truncate())
+        {
+            let (min_x, max_x) = (camera_boundary.min.x, camera_boundary.max.x);
+            let (min_y, max_y) = (camera_boundary.min.y, camera_boundary.max.y);
+            camera_transform.translation.x = camera_transform.translation.x.clamp(min_x, max_x);
+            camera_transform.translation.y = camera_transform.translation.y.clamp(min_y, max_y);
+        }
+
+        #[cfg(debug_assertions)]
+        {
+            gizmos.rect_2d(
+                camera_boundary.center(),
+                0.,
+                camera_boundary.size(),
+                Color::RED,
+            );
+            gizmos.circle_2d(camera_transform.translation.truncate(), 10., Color::RED);
+        }
+    }
 }
---
+++ src/level.rs
@@ -0,0 +1,137 @@
+use bevy::prelude::*;
+use noise::{NoiseFn, Perlin};
+use rand::random;
+
+use crate::AppState;
+
+pub const TILE_SIZE: Vec2 = Vec2::new(32., 32.);
+pub const GRID_SIZE: Vec2 = Vec2::new(100., 100.);
+pub const HALF_TILE_SIZE: Vec2 = Vec2::new(TILE_SIZE.x * 0.5, TILE_SIZE.y * 0.5);
+pub const HALF_GRID_SIZE: Vec2 = Vec2::new(GRID_SIZE.x * 0.5, GRID_SIZE.y * 0.5);
+
+pub struct LevelPlugin;
+
+impl Plugin for LevelPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(AppState::InGame), generate_level);
+
+        #[cfg(debug_assertions)]
+        {
+            app.add_systems(Update, debug_draw_tiles.after(generate_level));
+        }
+    }
+}
+
+#[cfg(debug_assertions)]
+fn debug_draw_tiles(
+    query: Query<(&Transform, Option<&BorderTile>), With<Tile>>,
+    mut gizmos: Gizmos,
+) {
+    for (transform, border_tile) in &query {
+        gizmos.rect_2d(
+            transform.translation.truncate(),
+            0.,
+            TILE_SIZE,
+            if border_tile.is_some() {
+                Color::BLACK
+            } else {
+                Color::FUCHSIA
+            },
+        );
+    }
+}
+
+fn generate_level(mut commands: Commands) {
+    const MAP_OFFSET_X: f64 = 0.;
+    const MAP_OFFSET_Y: f64 = 0.;
+    const MAP_SCALE: f64 = 20.;
+
+    let seed = random();
+    let perlin = Perlin::new(seed);
+    let tile_count = Tile::_LAST as u8;
+
+    for y in 0..GRID_SIZE.y as i32 {
+        for x in 0..GRID_SIZE.x as i32 {
+            let point = [
+                (x as f64 - MAP_OFFSET_X) / MAP_SCALE,
+                (y as f64 - MAP_OFFSET_Y) / MAP_SCALE,
+            ];
+            let noise_value = perlin.get(point).clamp(0., 1.);
+            let scaled_noise_value =
+                (noise_value * tile_count as f64).clamp(0., tile_count as f64 - 1.);
+            let int_noise_value = scaled_noise_value.floor() as u8;
+            let tile: Tile = int_noise_value.into();
+            let color = tile.into();
+            let custom_size = Some(TILE_SIZE);
+            let position = (Vec2::new(x as f32, y as f32) - HALF_GRID_SIZE) * TILE_SIZE;
+            let translation = position.extend(0.0);
+            let transform = Transform::from_translation(translation);
+
+            let mut tile_entity = commands.spawn(TileBundle {
+                sprite: SpriteBundle {
+                    sprite: Sprite {
+                        color,
+                        custom_size,
+                        ..default()
+                    },
+                    transform,
+                    ..default()
+                },
+                tile,
+            });
+
+            if y == 0 || x == 0 || y == GRID_SIZE.y as i32 - 1 || x == GRID_SIZE.x as i32 - 1 {
+                tile_entity.insert(BorderTile);
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct BorderTile;
+
+#[derive(Bundle)]
+pub struct TileBundle {
+    pub sprite: SpriteBundle,
+    pub tile: Tile,
+}
+
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Tile {
+    Water,
+    Sand,
+    Grass,
+    Hills,
+    Mountains,
+    _LAST,
+}
+
+impl From<u8> for Tile {
+    fn from(value: u8) -> Self {
+        // For every new type added to the enum, a new match arm should be added here.
+        match value {
+            0 => Self::Water,
+            1 => Self::Sand,
+            2 => Self::Grass,
+            3 => Self::Hills,
+            4 => Self::Mountains,
+            #[cfg(debug_assertions)]
+            _ => panic!("From<u8> for Tile: Missing match arm!"),
+            #[cfg(not(debug_assertions))]
+            _ => Self::Water,
+        }
+    }
+}
+
+impl From<Tile> for Color {
+    fn from(value: Tile) -> Self {
+        match value {
+            Tile::Grass => Self::DARK_GREEN,
+            Tile::Hills => Self::GRAY,
+            Tile::Mountains => Self::DARK_GRAY,
+            Tile::Water => Self::BLUE,
+            Tile::Sand => Self::BEIGE,
+            Tile::_LAST => Self::default(),
+        }
+    }
+}
