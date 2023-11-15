5b59291bad4c3e288281eba070b0a78573ed299c
mnmaita <47983254+mnmaita@users.noreply.github.com>
2023-11-16 00:14:18 +0100

    Adds Hitpoints module

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,11 +1,13 @@
 mod constants;
 mod enemy;
+mod hitpoints;
 mod level;
 mod player;
 mod plugin;
 
 pub use constants::*;
 pub use enemy::Enemy;
+pub use hitpoints::Hitpoints;
 pub use level::BorderTile;
 pub use player::Player;
 pub use plugin::GamePlugin;
