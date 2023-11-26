bc727d1ea2590c6e7035f33577dde2b6d0ac3b8a
mnmaita <47983254+mnmaita@users.noreply.github.com>
2023-11-26 21:45:20 +0100

    Refactors audio, fonts and textures modules to work with WASM builds

--- src/fonts.rs
+++ src/fonts.rs
@@ -10,7 +10,7 @@ pub struct FontsPlugin;
 impl Plugin for FontsPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<FontsLoadState>();
-        app.init_resource::<FontsFolderHandle>();
+        app.init_resource::<FontHandles>();
         app.add_systems(Startup, load_fonts);
         app.add_systems(
             Update,
@@ -20,34 +20,87 @@ impl Plugin for FontsPlugin {
 }
 
 #[derive(Resource, PartialEq)]
-struct FontsLoadState(RecursiveDependencyLoadState);
+enum FontsLoadState {
+    NotLoaded,
+    Loading,
+    Loaded,
+    Failed,
+}
 
 impl Default for FontsLoadState {
     fn default() -> Self {
-        Self(RecursiveDependencyLoadState::NotLoaded)
+        Self::NotLoaded
     }
 }
 
-impl FontsLoadState {
-    pub const LOADED: Self = Self(RecursiveDependencyLoadState::Loaded);
+impl From<RecursiveDependencyLoadState> for FontsLoadState {
+    fn from(value: RecursiveDependencyLoadState) -> Self {
+        match value {
+            RecursiveDependencyLoadState::NotLoaded => Self::NotLoaded,
+            RecursiveDependencyLoadState::Loading => Self::Loading,
+            RecursiveDependencyLoadState::Loaded => Self::Loaded,
+            RecursiveDependencyLoadState::Failed => Self::Failed,
+        }
+    }
 }
 
 #[derive(Resource, Default, Deref, DerefMut)]
-struct FontsFolderHandle(Handle<LoadedFolder>);
+struct FontHandles(
+    #[cfg(not(target_family = "wasm"))] Handle<LoadedFolder>,
+    #[cfg(target_family = "wasm")] Vec<Handle<Font>>,
+);
 
 fn load_fonts(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let fonts_folder_handle = asset_server.load_folder(ASSET_FOLDER_FONTS);
-    commands.insert_resource(FontsFolderHandle(fonts_folder_handle));
+    let font_handles = {
+        #[cfg(not(target_family = "wasm"))]
+        {
+            asset_server.load_folder(ASSET_FOLDER_FONTS)
+        }
+
+        #[cfg(target_family = "wasm")]
+        {
+            let asset_font_list = [
+                format!("{ASSET_FOLDER_FONTS}/MorrisRoman-Black.ttf"),
+                format!("{ASSET_FOLDER_FONTS}/MorrisRomanAlternate-Black.ttf"),
+                format!("{ASSET_FOLDER_FONTS}/Prince Valiant.ttf"),
+            ];
+            asset_font_list
+                .iter()
+                .map(|path| asset_server.load::<Font>(path))
+                .collect::<Vec<Handle<Font>>>()
+        }
+    };
+
+    commands.insert_resource(FontHandles(font_handles));
 }
 
 fn update_font_assets_load_state(
     mut fonts_load_state: ResMut<FontsLoadState>,
-    fonts_folder_handle: Res<FontsFolderHandle>,
+    font_handles: Res<FontHandles>,
     asset_server: Res<AssetServer>,
 ) {
-    fonts_load_state.0 = asset_server.recursive_dependency_load_state(fonts_folder_handle.id());
+    *fonts_load_state = {
+        #[cfg(not(target_family = "wasm"))]
+        {
+            asset_server
+                .recursive_dependency_load_state(font_handles.id())
+                .into()
+        }
+        #[cfg(target_family = "wasm")]
+        {
+            let all_loaded = font_handles.iter().all(|handle| {
+                asset_server.recursive_dependency_load_state(handle.id())
+                    == RecursiveDependencyLoadState::Loaded
+            });
+            if all_loaded {
+                RecursiveDependencyLoadState::Loaded.into()
+            } else {
+                RecursiveDependencyLoadState::NotLoaded.into()
+            }
+        }
+    };
 }
 
 pub fn font_assets_loaded() -> impl Condition<()> {
-    IntoSystem::into_system(resource_equals(FontsLoadState::LOADED))
+    IntoSystem::into_system(resource_equals(FontsLoadState::Loaded))
 }
